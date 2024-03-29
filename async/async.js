57be99ddf890d7dd6e76a182baac9c8f044ee41a
lipebol <lipe_bol5@hotmail.com>
2024-03-28 23:11:09 -0300

    Initial commit

---
+++ async/async.js
@@ -0,0 +1,20 @@
+function async() {
+    console.log('Começou!')
+    //console.log('Aguardando...')
+    fetch ('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+        if(response.status == 200) {
+            return response.json()
+        }
+    })
+    .then((json) => {
+        console.log(json[0])
+    })
+    .then(() => {
+        console.log('Terminou.')
+    })    
+    //console.log('Aguardando...')
+    
+}
+
+async()
---
+++ sync/sync.py
@@ -0,0 +1,18 @@
+from requests import get
+from json import loads
+
+
+def sync():
+    print('Começou!')
+    #print('Aguardando...')
+    #print('----------')
+    req = get('https://jsonplaceholder.typicode.com/posts')
+    if req.status_code == 200:
+        print('----------')
+        print(loads(req.text)[0])
+        print('----------')
+        print('Terminou.')
+        #print('----------')
+        #print('Aguardando...')
+
+sync()
