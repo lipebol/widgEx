bec82e7fa52d85f753a2a0d8cd43a330de752722
lipebol <lipe_bol5@hotmail.com>
2025-10-20 00:53:33 -0300

    Initial commit

---
+++ common/httpEx.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+from .loadEx import load
+from requests import get
+
+class httpEx:
+
+    @load.exception
+    @load.quiet
+    @staticmethod
+    def getdata(func):
+        def wrapper(**kwargs):
+            if (url := kwargs.get('url')):
+                load.info(f"Requesting... ({url})")
+                if (
+                    response := get(
+                        url, params=kwargs.get('params'), timeout=2, 
+                        headers={'User-Agent': load.variable('HEADERS')}
+                    )
+                ).status_code == 200:
+                    kwargs['response'] = response.text
+                    return func(**kwargs)
+                raise Exception(response.status_code, response.text)
+            raise Exception('URL?')
+        return wrapper
+    
+    @getdata
+    @staticmethod
+    def scrape(**kwargs) -> list:
+        if not kwargs.get('type') or kwargs.get('tag'):
+            kwargs['type'], kwargs['tag'] = ('lxml','a')
+        return BeautifulSoup(
+            kwargs.get('response'), kwargs.get('type') ### <-- 'xml' or 'lxml'
+        ).find_all(kwargs.get('tag'))
+        
+    @staticmethod
+    def checkIP(**kwargs):
+
+        @httpEx.getdata
+        def ip(**kwargs):
+            return kwargs.get('response').strip()
+        
+        if (ipadress := ip(url=load.variable('CHECK_IP'))):
+            if not load.checkpath(
+                tmpfile := load.tmpfile(path=load.path(join='common'))
+            ):
+                load.jsonEx(path=tmpfile, data={'ip': ipadress})
+            return load.jsonEx(path=tmpfile).get('ip') == ipadress
+        
