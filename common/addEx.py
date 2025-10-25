f95a35f9281b37df5bce0444c9c538f313585e89
lipebol <lipe_bol5@hotmail.com>
2025-10-24 23:07:00 -0300

    changed var name

--- common/addEx.py
+++ common/addEx.py
@@ -12,18 +12,18 @@ class add:
             try:
                 return func(*args, **kwargs)
             except Exception as error:
-                _projectname = load.path(
+                _widgExname = load.path(
                     getmodule(currentframe().f_back).__file__
                 ).name.strip('.py')
                 try:
                     system.notifysend(
-                        title=_projectname, message=notific.add_event(
-                            summary=f"{_projectname} ({type(error).__name__})", 
+                        title=_widgExname, message=notific.add_event(
+                            summary=f"{_widgExname} ({type(error).__name__})", 
                             description=format_exc(), colorId='11'
                         ).get('kind')
                     )
                 except Exception as error:
-                    system.notifysend(title=_projectname, message=format_exc())
+                    system.notifysend(title=_widgExname, message=format_exc())
         return wrapper
 
     
