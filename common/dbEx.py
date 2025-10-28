949bb879d52d69ab6d95e74b92ba9490c34459b1
lipebol <lipe_bol5@hotmail.com>
2025-10-28 02:06:33 -0300

    changed name and add new method

--- common/dbEx.py
+++ common/dbEx.py
@@ -1,3 +1,4 @@
+from .addEx import add
 from .loadEx import load, system
 from pymongo import MongoClient
 from pymongoarrow.monkey import patch_all
@@ -35,9 +36,19 @@ class mongodb:
         if not many:
             return mongodb.connect(database, collection).insert_one(data).inserted_id
 
+    @add.exception
     @staticmethod
-    def setdb(database: str):
-        load.variable('MONGODB_DEFAULT', add=database)
+    def setconfig(database: str | None = None):
+        if database:
+            load.variable('MONGODB_DEFAULT', add=database)
+            return list(mongodb().__env)
+
+    @property
+    def __env(self):
+        if not load.checkpath(tmpfile := load.tmpfile(path='/tmp')):
+            load.jsonEx(path=tmpfile, data=mongodb.select('_envs', database='common',)[0])
+        for key, value in load.jsonEx(path=tmpfile).items():
+            yield load.variable(key, add=value)
     
 
 
