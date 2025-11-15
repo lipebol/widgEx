b9b28b8c70987e72fa7ed286ac9a4efe4f3cdfbf
lipebol <lipe_bol5@hotmail.com>
2025-11-15 07:57:27 +0000

    add 'cryptHandler'

--- servEx/src/common/instances.js
+++ servEx/src/common/instances.js
@@ -1,16 +1,23 @@
+const { cryptHandler } = require(process.env.HANDLER_CRYPT)
 const { mongoose } = require(process.env.ODM)
 const { Sequelize, Op } = require(process.env.ORM)
 
-const mongooseSpotifEx = mongoose.createConnection(process.env.ENGINE_MONGODB).useDb('spotifEx')
+const mongooseSpotifEx = mongoose.createConnection(
+    cryptHandler(process.env.MONGODB_URI)
+).useDb('spotifEx')
 
 mongoose.set(
     'debug', (collection, method, query, agreggate, options) => {
-        console.log(JSON.stringify({ collection, method, query, agreggate, options }, null, 2))
+        console.log(
+            JSON.stringify(
+                { collection, method, query, agreggate, options }, null, 2
+            )
+        )
     }
 )
 
 const sequelizeConnect = new Sequelize(
-    process.env.ENGINE_POSTGRES,
+    process.env.POSTGRES_URI,
     {
         dialect: process.env.DIALECT,
         //port: parseInt(process.env.POSTGRES_PORT)
