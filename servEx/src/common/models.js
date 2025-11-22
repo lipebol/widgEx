c3b500f565b6e86849d3bd66ebb571649e1dda59
lipebol <lipe_bol5@hotmail.com>
2025-11-22 05:09:22 +0000

    add modern mode

--- servEx/src/common/models.js
+++ servEx/src/common/models.js
@@ -1,7 +1,8 @@
-const { mongoose, mongooseSpotifEx } = require(process.env.INSTANCES)
+import { mongoose } from 'mongoose'
+import { mongooseSpotifEx } from './instances.js'
 
-const SPOTIFEX_GENRES = mongooseSpotifEx.model(
-    'SPOTIFEX_GENRES', new mongoose.Schema(
+export const spotifExGenres = mongooseSpotifEx.model(
+    'spotifExGenres', new mongoose.Schema(
         {
             id: { type: mongoose.ObjectId, required: true },
             name: { type: String, required: true },
@@ -10,44 +11,42 @@ const SPOTIFEX_GENRES = mongooseSpotifEx.model(
     ), 'genres'
 )
 
-const SPOTIFEX_ARTISTS = mongooseSpotifEx.model(
-    'SPOTIFEX_ARTISTS', new mongoose.Schema(
+export const spotifExArtists = mongooseSpotifEx.model(
+    'spotifExArtists', new mongoose.Schema(
         {
             id: { type: mongoose.ObjectId, required: true },
             name: { type: String, required: true },
             profile: { type: String, required: true },
-            genres: [{ type: mongoose.ObjectId, ref: 'SPOTIFEX_GENRES' }]
+            genres: [{ type: mongoose.ObjectId, ref: 'spotifExGenres' }]
         }
     ), 'artists'
 )
 
-const SPOTIFEX_TRACKS = mongooseSpotifEx.model(
-    'SPOTIFEX_TRACKS', new mongoose.Schema(
+export const spotifExTracks = mongooseSpotifEx.model(
+    'spotifExTracks', new mongoose.Schema(
         {
             id: { type: mongoose.ObjectId, required: true },
             trackid: { type: String, required: true },
             length: { type: Number, required: true },
             artUrl: { type: String, required: true },
             album: { type: String, required: true },
-            artist: { type: mongoose.ObjectId, ref: 'SPOTIFEX_ARTISTS' },
+            artist: { type: mongoose.ObjectId, ref: 'spotifExArtists' },
             autoRating: { type: Number, required: true },
             discNumber: { type: String, required: true },
-            name: { type: String, required: true },
+            title: { type: String, required: true },
             trackNumber: { type: Number, required: true },
             url: { type: String, required: true }
         }
     ), 'tracks'
 )
 
-const SPOTIFEX_DAYLISTS = mongooseSpotifEx.model(
-    'SPOTIFEX_DAYLISTS', new mongoose.Schema(
+export const spotifExDaylists = mongooseSpotifEx.model(
+    'spotifExDaylists', new mongoose.Schema(
         {
             id: { type: mongoose.ObjectId, required: true },
-            track: { type: mongoose.ObjectId, ref: 'SPOTIFEX_TRACKS' },
+            track: { type: mongoose.ObjectId, ref: 'spotifExTracks' },
             date: { type: String, required: true },
             listen: { type: Number, required: true }
         }
     ), 'daylists'
 )
-
-module.exports = { SPOTIFEX_GENRES, SPOTIFEX_ARTISTS, SPOTIFEX_TRACKS, SPOTIFEX_DAYLISTS }
