758a0c15b3b2c79936ad4cdb23324dfb647932a6
lipebol <lipe_bol5@hotmail.com>
2025-11-22 05:08:42 +0000

    add modern mode

--- servEx/src/common/instances.js
+++ servEx/src/common/instances.js
@@ -1,10 +1,6 @@
-const { cryptHandler } = require(process.env.HANDLER_CRYPT)
-const { mongoose } = require(process.env.ODM)
-const { Sequelize, Op } = require(process.env.ORM)
-
-const mongooseSpotifEx = mongoose.createConnection(
-    cryptHandler(process.env.MONGODB_URI)
-).useDb('spotifEx')
+import { cryptHandler } from './handlers/crypt.js'
+import { mongoose } from 'mongoose'
+import { Sequelize } from 'sequelize'
 
 mongoose.set(
     'debug', (collection, method, query, agreggate, options) => {
@@ -16,12 +12,14 @@ mongoose.set(
     }
 )
 
-const sequelizeConnect = new Sequelize(
+export const mongooseSpotifEx = mongoose.createConnection(
+    cryptHandler(process.env.MONGODB_URI)
+).useDb('spotifEx')
+
+export const sequelizeConnect = new Sequelize(
     process.env.POSTGRES_URI,
     {
         dialect: process.env.DIALECT,
         //port: parseInt(process.env.POSTGRES_PORT)
     }
 )
-
-module.exports = { mongoose, mongooseSpotifEx, Sequelize, sequelizeConnect, Op }
