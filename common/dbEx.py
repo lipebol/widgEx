e408929c2ea0c33eb36f3ac7682f4128a0e051ca
lipebol <lipe_bol5@hotmail.com>
2025-11-27 15:52:41 -0300

    add connection

--- common/dbEx.py
+++ common/dbEx.py
@@ -1,7 +1,41 @@
 from .addEx import add
 from .loadEx import load, system
+import adbc_driver_postgresql.dbapi
 from pymongo import MongoClient
 from pymongoarrow.monkey import patch_all
+
+class db:
+ 
+    @property
+    def connect(self):
+        return adbc_driver_postgresql.dbapi.connect()
+    
+    @staticmethod
+    def columns(schema: str, table: str):
+        with db().conn().cursor() as __cursor:
+            __cursor.execute(
+                load.variable('SELECT_COMMAND') % (schema, table)
+            )
+            return [column[0] for column in __cursor.description]
+
+    @staticmethod
+    def select(schema: str, table: str, params=None):
+        sql_command = load.variable('SELECT_COMMAND') % (schema, table)
+        if params:
+            pass
+        return db().__postgres.get_records(sql_command)
+
+    @staticmethod
+    def adbc(pyarrowTable: object, schema: str, table: str) -> str:
+        with __conn.cursor() as cursor:
+            cursor.adbc_ingest(
+                db_schema_name=schema, table_name=table, 
+                data=pyarrowTable, mode='append'
+            )
+        __conn.commit()
+        __conn.close()
+        return load.variable('MESSAGE_SUCCESS')
+
 
 class mongodb:
 
