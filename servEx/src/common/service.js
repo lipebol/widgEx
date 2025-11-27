ed5416f9db5384f1ff0d31df4db74984a466132f
lipebol <lipe_bol5@hotmail.com>
2025-11-27 04:04:25 +0000

    rewritten to 'mongodb'

--- servEx/src/common/service.js
+++ servEx/src/common/service.js
@@ -1,41 +1,9 @@
-const { Sequelize, Op } = require(process.env.INSTANCES)
-const {
-     SPOTIFEX_GENRES, SPOTIFEX_ARTISTS, 
-     SPOTIFEX_TRACKS, SPOTIFEX_DAYLISTS 
-} = require(process.env.MODELS)
+import { Op } from 'sequelize'
+import * as models from './models.js'
 
+export class Service {
 
-class Service {
-
-    constructor() {
-        this.newObject = () => { return JSON.parse(process.env.OBJECT_EMPTY) }
-        this.setModel = (model) => { return eval(model) }
-        this.setLimit = parseInt(process.env.LIMIT)
-        this.setOffSet = (page, limit) => { try { if (page && limit) { return (page - 1) * limit } } catch (err) { console.log(err) } }
-        this.setParams = (params, newObject) => {
-            try {
-                if (params.dates) {
-                    [newObject.start, newObject.end] = params.dates
-                        .split(process.env.SEPARATOR)
-                    newObject.end = newObject.end.concat(
-                        process.env.SPACE, process.env.HOUR_END
-                    )
-                } else if (params.multi) {
-                    newObject.multi = params.multi
-                }
-                return newObject
-            } catch (err) { console.log(err) }
-        }
-
-        this.counter = (rows, newObject) => {
-            newObject.total_registros = rows
-            newObject.paginas = Math.ceil(parseFloat(rows) / this.setLimit)
-            return newObject
-        }
-    }
-
-
-    async sql(object) {
+    static async sql(dataobject) {
         try {
 
             this.sequelize = new setSequelize(this.newObject())
@@ -46,7 +14,7 @@ class Service {
 
             if (object.params) {
                 if (object.params.dates === process.env.MAX) {
-                    return await this.setModel(object.model).max(object.filter)
+                    return await models[object.model].max(object.filter)
                 }
                 this.sequelize.where(object.filter, this.setParams(object.params, this.newObject()))
             }
@@ -55,30 +23,39 @@ class Service {
             if (object.page) {
                 this.sequelize.offset(this.setOffSet(object.page, this.setLimit))
                 this.sequelize.limit(this.setLimit)
-            } else if (object.count) {
-                return this.counter(
-                    await this.setModel(object.model)
-                        .count(this.sequelize.finally()), this.newObject()
-                )
+            } else if (dataobject.count) {
+                dataobject.count = await models[dataobject.model]
+                    .count(this.sequelize.finally())
+                return {
+                    total: dataobject.count,
+                    pages: Math.ceil(parseFloat(rows) / dataobject.limit)
+                }
             }
 
-            return await this.setModel(object.model).findAll(this.sequelize.finally())
+            return await models[object.model].findAll(this.sequelize.finally())
         } catch (err) { console.log(err) }
     }
 
 
-    async mongodb(object) {
+    static async nosql(dataobject) {
         try {
+            const data = await models[dataobject.model]
+                .find(dataobject.where).sort({ [dataobject.filter]: 'asc' })
+                .select(dataobject.fields).skip(dataobject.offset)
+                .limit(dataobject.limit).populate(dataobject.lookup).exec()
 
-            this.mongoose = new setMongoose(
-                object.filter, object.params ?
-                this.setParams(object.params, this.newObject()) : undefined
-            )
+            if (data.length === 0) {
+                dataobject.error = { name: 'NotFound', code: 404 }
+            }
 
-            return await this.setModel(object.model)
-                .find(this.mongoose.where()).sort(this.mongoose.sort())
-                .select(object.fields).skip(this.setOffSet(object.page, this.setLimit))
-                .limit(this.setLimit).populate(object.lookup).exec()
+            if (!dataobject.error && dataobject.info) {
+                return {
+                    count: data.length, countpages: Math.ceil(
+                        parseFloat(data.length) / 100
+                    )
+                }
+            }
+            return data
         } catch (err) { console.log(err) }
     }
 }
@@ -103,24 +80,3 @@ class setSequelize {
 }
 
 
-class setMongoose {
-    constructor(filter, params) {
-        this.filter = filter
-        this.params = params
-    }
-    where() {
-        try {
-            if (this.filter && this.params) {
-                return Object.keys(this.params).length === 1 ?
-                    { [this.filter]: { '$in': this.params.multi } } :
-                    { [this.filter]: { '$gte': new Date(this.params.start), '$lte': new Date(this.params.end) } }
-            }
-        } catch (err) { console.log(err) }
-    }
-    sort() { try { if (this.filter) { return { [this.filter]: 'asc' } } } catch (err) { console.log(err) } }
-}
-
-
-const service = new Service()
-
-module.exports = service
