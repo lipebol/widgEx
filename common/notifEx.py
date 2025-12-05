fc024595173ec423568f477e98a55afaf18497cb
lipebol <lipe_bol5@hotmail.com>
2025-12-05 12:45:28 -0300

    add new method 'exception' and changed param 'date' in 'event' method

--- common/notifEx.py
+++ common/notifEx.py
@@ -1,19 +1,39 @@
-from .authEx import authentication
+from .authEx import auth
 from .loadEx import load, system
 from .mountEx import mount
+from inspect import currentframe
+from traceback import format_exc
 
 
 class notific:
+
+    @staticmethod
+    def exception(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as error:
+                try:
+                    if (widgEx := load.widgex(currentframe().f_back)):
+                        system.notifysend(
+                            title=widgEx, message=notific.event(
+                                summary=f"{widgEx} ({type(error).__name__})", 
+                                description=format_exc(), colorId='11'
+                            ).get('kind')
+                        )
+                except Exception as error:
+                    system.notifysend(title=widgEx, message=format_exc())
+        return wrapper
     
     @staticmethod
-    def add_event(
+    def event(
         *, summary: str, description: str, colorId: str, 
-        date = mount.data(classname='Event_Date')
+        date = mount.data(dateTime=load.now())
     ):
-        return authentication.service(
+        return auth.googleservice(
             name='calendar', version='v3', 
-            auth=load.variable('NOTIFIC_AUTH'), 
-            scope=load.variable('NOTIFIC_SCOPE')
+            _auth=load.variable('NOTIFIC_AUTH'), 
+            _scope=load.variable('NOTIFIC_SCOPE')
         ).events().insert(
             calendarId=system.decr(variable=load.variable('NOTIFIC_ID')), 
             body=mount.data(
