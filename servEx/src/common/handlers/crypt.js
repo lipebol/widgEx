51f535348c4b32eab29af6c07d6f0fd380745762
lipebol <lipe_bol5@hotmail.com>
2025-12-10 19:34:10 -0300

    merged in 'datarized'

--- servEx/src/common/handlers/crypt.js
+++
@@ -1,8 +0,0 @@
-import { spawnSync } from 'child_process'
-
-export const cryptHandler = (uri) => {
-    return spawnSync(
-        `clevis-decrypt-tang < ${uri}`, 
-        { shell: true, encoding: 'utf8' }
-    ).stdout.trim()
-}
